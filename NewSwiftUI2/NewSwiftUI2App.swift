d5665e2d227d8e9918cdcba45dc131b67026e977
Mohammed Abunada <m7606225@gmail.com>
2020-06-27 18:49:42 +0200

    Initial Commit

---
+++ NewSwiftUI2/NewSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftUI2App.swift
+//  NewSwiftUI2
+//
+//  Created by Mohammed Abunada on 2020-06-27.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
