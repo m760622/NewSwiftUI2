b4b1a3f3cb4b8557d4bdcc95041bc06df235c0a7
Mohammed Abunada <m7606225@gmail.com>
2020-06-29 22:55:23 +0200

    New LazyVStack and  LazyHStack View

--- NewSwiftUI2/LazyVHStackView.swift
+++ NewSwiftUI2/LazyVHStackView.swift
@@ -1,20 +1,59 @@
-//
+// https://github.com/m760622/NewSwiftUI2/blob/main/NewSwiftUI2/LazyVHStackView.swift
 //  LazyVHStackView.swift
 //  NewSwiftUI2
-//
+// m7606225@gmail.com
 //  Created by Mohammed Abunada on 2020-06-29.
-//
+// https://github.com/m760622
 
 import SwiftUI
 
 struct LazyVHStackView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack{
+                ScrollView {
+                    LazyVStack {
+                        ForEach(1...1000, id: \.self, content: SampleRow.init)
+                    }//LazyVStack
+                    .padding(.vertical, 10)
+                }//ScrollView
+                
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 10) {
+                        ForEach(0..<1000) { index in
+                            Text("\(index)")
+                                .frame(width: 30, height: 300)
+                                .background(Color.blue.opacity(0.9))
+                                .clipShape(Capsule())
+                        }
+                    }//LazyHStack
+                    .padding(.horizontal, 10)
+                }//ScrollView
+            }//VStack
+            .navigationBarTitle(Text("New LazyVStack and  LazyHStack View"), displayMode: .inline)
+        }//NavigationView
+    }//body
+}//LazyVHStackView
+
+struct SampleRow: View {
+    let id: Int
+    var body: some View {
+        Text("\(id)")
+            .frame(width: 300, height: 30)
+            .background(Color.blue.opacity(0.7))
+            .clipShape(Capsule())
+            .padding(.vertical, 5)
     }
-}
+    
+    init(id: Int) {
+        print("Loading row \(id)")
+        self.id = id
+    }
+}//SampleRow
 
 struct LazyVHStackView_Previews: PreviewProvider {
     static var previews: some View {
         LazyVHStackView()
     }
 }
+
